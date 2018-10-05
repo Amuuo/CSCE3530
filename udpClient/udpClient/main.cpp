a35500a4e913034773db9bb872201151071ce232
adam <adamthewilliams@gmail.com>
2018-10-04 19:32:37 -0500

    functional

--- udpClient/udpClient/main.cpp
+++ udpClient/udpClient/main.cpp
@@ -17,35 +17,46 @@ struct sockaddr_in SVR_ADDR;
 using namespace std;
 
 int main(int argc, char** argv){
-	
-	if(argc < 3 || argc > 3){
-		cout << "\nUsage: <ip address> <port number>";
-	}
+
 	
 	string message;
 	char   msgBuffer[256];
+  socklen_t  msgLength;
 	
-	SOCK = socket(AF_INET, SOCK_DGRAM, 0);
-	
+
 	SVR_ADDR.sin_family = AF_INET;
-	SVR_ADDR.sin_addr.s_addr = argv[1];
-	SVR_ADDR.sin_port = htons(stoi(argv[2]));
+	SVR_ADDR.sin_addr.s_addr = INADDR_ANY;
+	SVR_ADDR.sin_port = htons(stoi(argv[1]));
+
+	
+  
+  SOCK = socket(AF_INET, SOCK_DGRAM, 0);
+	
 	
 	
 	while(1){
 		
 		cout << "\nEnter message: ";
-		cin >> message;
+		getline(cin, message);    
 		
 		sendto(SOCK, 
-               (char*)&message.c_str(), 
-               message.size(), 
-               MSG_CONFIRM, 
-               (struct sockaddr*)&SVR_ADDR, 
-               // ENTER MSGLENGTH
-               );
+           (char*)message.c_str(), 
+           message.size()+1, 
+           MSG_CONFIRM, 
+           (struct sockaddr*)&SVR_ADDR, 
+           sizeof(SVR_ADDR));
 			   
-		recvfrom(SOCK, (
+		recvfrom(SOCK, 
+             (char*)msgBuffer, 
+             256, 
+             MSG_WAITALL, 
+             (struct sockaddr*)&SVR_ADDR, 
+             &msgLength);
+
+    cout << "\nServer: " << msgBuffer;
+
+    memset(msgBuffer, 0, 256);
+    message.clear();
 	}
 	
 	
--- udpServer/udpServer/main.cpp
+++ udpServer/udpServer/main.cpp
@@ -8,72 +8,83 @@
 #include <netinet/in.h>
 #include <string>
 #include <algorithm>
+#include <iostream>
+#include <assert.h>
 
 int SOCK;
 int PORT;
 struct sockaddr_in SVR_ADDR;
 struct sockaddr_in CLI_ADDR;
 
-using std::string;
-using std::cout;
-using std::transform;
+using namespace std;
+
+
+
 
 int main(int argc, char** argv){
 	
-	// first arg should be port #
-	if(argc < 2 || argc > 2){
-		cout << "\nUsage: <port number>";
-	}
 	
-	PORT = stoi(argv[1]);
+	//PORT = stoi(argv[1]);
 	
-	int    msgLength;
+	socklen_t msgLength = sizeof(sockaddr);
 	int    msgLength2;
 	char   msgBuffer[256];
 	string msgBufferStr;
 	
 	
-	// create socket
-	SOCK = socket(AF_INET, SOCK_DGRAM, 0);
-	
 	SVR_ADDR.sin_family      = AF_INET;
 	SVR_ADDR.sin_addr.s_addr = INADDR_ANY;
-	SVR_ADDR.sin_port        = htons(PORT);
+	SVR_ADDR.sin_port        = htons(stoi(argv[1]));
+	
+  
+  
+  // create socket
+	SOCK = socket(AF_INET, 
+                SOCK_DGRAM, 
+                0);
+	cout << "\nSocket created...";
 
-	// bind to socket
-	bind(SOCK, (struct sockaddr*)&SVR_ADDR, sizeof(SVR_ADDR));
+  // bind to socket
+	bind(SOCK, 
+       (struct sockaddr*)&SVR_ADDR, 
+       sizeof(SVR_ADDR));
 	
+  cout << "\nBound to socket...\nWaiting for messages from client...";
+
+
 	while(1){
-	
+	    
 		// receive message from client
 		msgLength2 = recvfrom(SOCK, 
-                              (char*)msgBuffer, 
-                              256,
-                              MSG_WAITALL,
-                              (struct sockaddr*)&CLI_ADDR, 
-                              &msgLength);
+                          (char*)msgBuffer, 
+                          256,
+                          MSG_WAITALL,
+                          (struct sockaddr*)&CLI_ADDR, 
+                          &msgLength);
 	
 		msgBuffer[msgLength2] = '\0';
-		
-		// copy cstring to std::string and transform chars		
-		msgBufferStr = string{msgBuffer};		
-		transform(msgBufferStr.begin(), 
-                  msgBufferStr.end(), 
-                  msgBufferStr.begin(), 
-                  ::toupper);
-				  
 		cout << "\nClient: " << msgBuffer;
-		cout << "\nServer: " << msgBufferStr;
 		
-		
+		// copy cstring to std::string and transform chars				
+    msgBufferStr = string{msgBuffer};		
+		transform(msgBufferStr.begin(), 
+              msgBufferStr.end(), 
+              msgBufferStr.begin(), 
+              ::toupper);
+				  		
 		// send transformed string to client
 		sendto(SOCK, 
-               (char*)&msgBufferStr.c_str(), 
-               msgBufferStr.size(),
-               MSG_CONFIRM,
-               (struct sockaddr*)&CLI_ADDR,
-               msgLength);			   
+           (char*)msgBufferStr.c_str(), 
+           msgBufferStr.size() + 1,
+           MSG_CONFIRM,
+           (struct sockaddr*)&CLI_ADDR,
+           msgLength);			   
+		
+    cout << "\nServer: " << msgBufferStr;
 	}
+
+  memset(msgBuffer, 0, 256);
+  msgBufferStr.clear();
 	
 	return 0;
 }
